f988b38481ef96dd85cdd8045a56e38ab4722c4e
Sabrina <sabrinah_38@hotmail.fr>
2025-06-23 12:07:32 +0200

    add logic

--- components/keyModule/useKeyModule.ts
+++ components/keyModule/useKeyModule.ts
@@ -1,1 +1,42 @@
-// partie algorithme
+export const COLUMNS: string[][] = [
+
+  [ 'image_1',  'image_8',  'image_9',  'image_12' ],
+  [ 'image_2',  'image_5',  'image_11', 'image_14' ],
+  [ 'image_3',  'image_6',  'image_10', 'image_15' ],
+  [ 'image_4',  'image_7',  'image_13', 'image_16' ],
+  [ 'image_17', 'image_18', 'image_19', 'image_20' ],
+  [ 'image_21', 'image_22', 'image_23', 'image_24' ],
+  [ 'image_25', 'image_26' ],
+];
+
+
+export function solveKeypad(selectedSymbols: string[]): string[] {
+  if (selectedSymbols.length !== 4) {
+    throw new Error('Vous devez sélectionner exactement 4 symboles.');
+  }
+
+  for (let colIndex = 0; colIndex < COLUMNS.length; colIndex++) {
+    const currentColumn = COLUMNS[colIndex];
+
+    const allPresent = selectedSymbols.every(symbol =>
+      currentColumn.includes(symbol)
+    );
+
+    if (allPresent) {
+  
+      const orderedResult: string[] = [];
+      for (let i = 0; i < currentColumn.length; i++) {
+        const symbol = currentColumn[i];
+        if (selectedSymbols.includes(symbol)) {
+          orderedResult.push(symbol);
+        }
+      }
+
+      return orderedResult;
+    }
+  }
+
+  throw new Error(
+    'Aucune colonne ne contient tous les symboles sélectionnés.'
+  );
+}
